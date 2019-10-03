bc1e06cd6d51891fda7ee371995a448c81cb5b4f
vjc66 <55262819+vjc66@users.noreply.github.com>
2019-10-03 14:42:51 -0400

    slingshot

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 // YOU must implement the Slingshot
 
 public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
 
-
-    void OnMouseEnter()
-    {
-        print("Slingshot:OnMouseEnter()");
+    void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");            // a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);                                         // b
     }
 
-    void OnMouseExit()
-    {
-        print("Slingshot:OnMouseExit()");
+    void OnMouseEnter() {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);                                           // b
+    }
+
+    void OnMouseExit() {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);                                          // b
     }
 
 }
-
